f36275eab623ab05c3cbf3bc4b151b2261297b27
kiaczb <b3n3kin@gmail.com>
2026-03-30 19:49:34 +0200

    Fixed getting competitors by the reference competition date

--- queries/personal/get-registered-competitors-last-competition-by-months.sql
+++ queries/personal/get-registered-competitors-last-competition-by-months.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT p.name AS person_name,
        c.cell_name AS last_competition,
-       c.start_date,
+       t.last_date AS last_competition_date,
        TIMESTAMPDIFF(MONTH, t.last_date, ref.start_date) AS months_inactive
 FROM (
     SELECT r.person_id,
@@ -9,15 +9,16 @@ FROM (
     JOIN users u ON u.id = reg.user_id
     JOIN results r ON r.person_id = u.wca_id
     JOIN competitions c ON c.id = r.competition_id
+    JOIN competitions ref ON ref.id = ':COMPETITION_ID'
     WHERE reg.competing_status = 'accepted'
       AND reg.is_competing = 1
       AND reg.competition_id = ':COMPETITION_ID'
+      AND c.start_date < ref.start_date
     GROUP BY r.person_id
 ) t
 JOIN results r ON r.person_id = t.person_id
-JOIN competitions c ON c.id = r.competition_id
+JOIN competitions c ON c.id = r.competition_id AND c.start_date = t.last_date
 JOIN persons p ON p.wca_id = r.person_id
 JOIN competitions ref ON ref.id = ':COMPETITION_ID'
-WHERE c.start_date = t.last_date
-  AND TIMESTAMPDIFF(MONTH, t.last_date, ref.start_date) >= :MONTHS_INACTIVE
-ORDER BY c.start_date ASC;
+WHERE TIMESTAMPDIFF(MONTH, t.last_date, ref.start_date) >= :MONTHS_INACTIVE
+ORDER BY t.last_date ASC;
